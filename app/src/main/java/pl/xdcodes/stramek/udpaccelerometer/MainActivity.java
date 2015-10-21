003c81e07e52fcfd2e4aa217ef4ccc52ff75400e
stramek <Stramek@MacBook-Pro-Marcin.local>
2015-10-21 22:48:14 +0200

    ScrollView + magnetometr + poprawki

--- app/src/main/java/pl/xdcodes/stramek/udpaccelerometer/MainActivity.java
+++ app/src/main/java/pl/xdcodes/stramek/udpaccelerometer/MainActivity.java
@@ -17,11 +17,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = MainActivity.class.getName();
+
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
+    private Sensor senMagnetometer;
+
     private TextView accelerometerX;
     private TextView accelerometerY;
     private TextView accelerometerZ;
+    private TextView magnetometerA;
+    private TextView magnetometerB;
+    private TextView magnetometerC;
+
     private FloatingActionButton fab;
     private long lastUpdate = 0;
 
@@ -35,12 +42,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Context context = getApplicationContext();
         senSensorManager = (SensorManager) getSystemService(context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senMagnetometer = senSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         accelerometerX = (TextView) findViewById(R.id.accelerometerX);
         accelerometerY = (TextView) findViewById(R.id.accelerometerY);
         accelerometerZ = (TextView) findViewById(R.id.accelerometerZ);
+        magnetometerA = (TextView) findViewById(R.id.magnetometerA);
+        magnetometerB = (TextView) findViewById(R.id.magnetometerB);
+        magnetometerC = (TextView) findViewById(R.id.magnetometerC);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +92,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -87,19 +100,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor mySensor = event.sensor;
 
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
+            float[] accelerometer = event.values;
 
             Long curTime = System.currentTimeMillis();
-
             if ((curTime - lastUpdate) > 100) {
                 lastUpdate = curTime;
 
-                accelerometerX.setText(String.valueOf(Math.round(x * 100.0) / 100.0));
-                accelerometerY.setText(String.valueOf(Math.round(y * 100.0) / 100.0));
-                accelerometerZ.setText(String.valueOf(Math.round(z * 100.0) / 100.0));
+                accelerometerX.setText(String.valueOf(Math.round(accelerometer[0] * 100.0) / 100.0));
+                accelerometerY.setText(String.valueOf(Math.round(accelerometer[1] * 100.0) / 100.0));
+                accelerometerZ.setText(String.valueOf(Math.round(accelerometer[2] * 100.0) / 100.0));
             }
+        }
+
+        if (mySensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            float[] magnetometer = event.values;
+
+            //Long curTime = System.currentTimeMillis();
+            //if ((curTime - lastUpdate) > 100) {
+              //  lastUpdate = curTime;
+
+                magnetometerA.setText(String.valueOf(Math.round(magnetometer[0] * 100.0) / 100.0));
+                magnetometerB.setText(String.valueOf(Math.round(magnetometer[1] * 100.0) / 100.0));
+                magnetometerC.setText(String.valueOf(Math.round(magnetometer[2] * 100.0) / 100.0));
+           // }
         }
     }
 
